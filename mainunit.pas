2375a75bdcdd361b0b81e9fadcde25217e4e0e73
Oleksii Yefremenko <oleksii.yefremenko@tmx-learning.com>
2026-01-24 19:52:33 +0200

    Add project files

---
+++ mainunit.pas
@@ -0,0 +1,412 @@
+unit MainUnit;
+
+{$MODE DELPHI}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, ActnList, Menus,
+  ComCtrls, StdActns, ExtCtrls, TAGraph, TAFuncSeries, TASeries, TASources,
+  fpSpreadsheetCtrls, fpSpreadsheetGrid, fpsallformats,
+  Grids, StdCtrls, fpSpreadsheet, fpsTypes, fpsUtils, Generics.Collections;
+
+const
+  PANEL_SELECTED_RANGE = 0;
+  PANEL_OUTPUT_CELL = 1;
+
+  MODE_NONE = 0;
+  MODE_MOVING_AVERAGE = 1;
+  MODE_SIMPLE_EXPONENTIAL = 2;
+  MODE_MEDIAN = 3;
+
+type
+  TDoubleArray = array of Double;
+
+  { TMainForm }
+
+  TMainForm = class(TForm)
+    MenuItem4: TMenuItem;
+    SaveFile: TAction;
+    ActionList1: TActionList;
+    Label1: TLabel;
+    ToolButton4: TToolButton;
+    WriteButton: TButton;
+    Chart1: TChart;
+    GetSelectedDataMenuItem: TMenuItem;
+    SmoothedDataSource: TListChartSource;
+    OriginalDataSource: TListChartSource;
+    Panel1: TPanel;
+    MovingAverageRadioButton: TRadioButton;
+    ExponentialRadioButton: TRadioButton;
+    MedianRadioButton: TRadioButton;
+    SelectOutputCellMenuItem: TMenuItem;
+    OriginalData: TLineSeries;
+    MovingAverageSpan: TTrackBar;
+    ExponentialSpan: TTrackBar;
+    MedianSpan: TTrackBar;
+    WorksheetGridPopupMenu: TPopupMenu;
+    SmoothedData: TLineSeries;
+    FileExit: TFileExit;
+    FileOpen: TFileOpen;
+    ImageList1: TImageList;
+    MainMenu1: TMainMenu;
+    MenuItem1: TMenuItem;
+    MenuItem2: TMenuItem;
+    MenuItem3: TMenuItem;
+    PageControl1: TPageControl;
+    Separator1: TMenuItem;
+    StatusBar: TStatusBar;
+    sWorkbookTabControl1: TsWorkbookTabControl;
+    WorkbookSource: TsWorkbookSource;
+    WorksheetGrid: TsWorksheetGrid;
+    TabSheet1: TTabSheet;
+    TabSheet2: TTabSheet;
+    ToolBar: TToolBar;
+    ToolButton1: TToolButton;
+    ToolButton2: TToolButton;
+    ToolButton3: TToolButton;
+    procedure ExponentialRadioButtonChange(Sender: TObject);
+    procedure ExponentialRadioButtonClick(Sender: TObject);
+    procedure ExponentialSpanChange(Sender: TObject);
+    procedure FileOpenAccept(Sender: TObject);
+    procedure GetSelectedDataMenuItemClick(Sender: TObject);
+    procedure MedianRadioButtonChange(Sender: TObject);
+    procedure MedianRadioButtonClick(Sender: TObject);
+    procedure MedianSpanChange(Sender: TObject);
+    procedure MovingAverageRadioButtonChange(Sender: TObject);
+    procedure MovingAverageRadioButtonClick(Sender: TObject);
+    procedure MovingAverageSpanChange(Sender: TObject);
+    procedure SaveFileExecute(Sender: TObject);
+    procedure SelectOutputCellMenuItemClick(Sender: TObject);
+    procedure WorksheetGridTopLeftChanged(Sender: TObject);
+    procedure WriteButtonClick(Sender: TObject);
+  private
+    procedure MovingAverageMethod(const InputData: TDoubleArray; var OutputData: TDoubleArray; const Span: Integer);
+    procedure SimpleExponentialMethod(const InputData: TDoubleArray; var OutputData: TDoubleArray; const Alpha: Real);
+    procedure MedianMethod(const InputData: TDoubleArray; var OutputData: TDoubleArray; const Span: Integer);
+  public
+    FileName: String;
+    OriginalDataArray: array of array of Double;
+    SmoothingMode: Integer;
+    OutputCell: TGridRect;
+    SmoothedY: TDoubleArray;
+    procedure redrawChart();
+    procedure updateSmoothedChart();
+  end;
+
+var
+  MainForm: TMainForm;
+
+implementation
+
+{$R *.lfm}
+
+{ TMainForm }
+
+procedure TMainForm.WorksheetGridTopLeftChanged(Sender: TObject);
+begin
+  MainForm.WorksheetGrid.Invalidate;
+end;
+
+procedure TMainForm.WriteButtonClick(Sender: TObject);
+var
+  Selection: TGridRect;
+  Sheet: TsWorksheet;
+  row, col, SelectedColsAmount, SelectedRowsAmount, rows, i: Integer;
+  Cell: PCell;
+  SelectionRange: string;
+begin
+  Sheet := WorksheetGrid.WorkbookSource.worksheet;
+  if (not Sheet.IsEmpty) then begin
+    rows := Length(OriginalDataArray);
+    row := OutputCell.Top - 1;
+    col := OutputCell.Left - 1;
+    for i := 0 to rows - 1 do begin
+      Sheet.WriteNumber(row, col, SmoothedY[i]);
+      inc(row);
+    end;
+
+    //Selection := WorksheetGrid.Selection;
+    //SelectedColsAmount := Selection.Right - Selection.Left + 1;
+    //SelectedRowsAmount := Selection.Bottom - Selection.Top + 1;
+    //if SelectedColsAmount = 2 then
+    //begin
+    //  MainForm.OriginalDataArray := nil;
+    //  SetLength(MainForm.OriginalDataArray, SelectedRowsAmount, 2);
+    //  for row := Selection.Top to Selection.Bottom do
+    //    for col := Selection.Left to Selection.Right do
+    //    begin
+    //      Cell := Sheet.FindCell(row - 1 , col - 1);
+    //      if (Cell <> nil) and (Cell^.ContentType = cctNumber) then
+    //      begin
+    //        MainForm.OriginalDataArray[row - Selection.Top, col - Selection.Left] := Cell^.NumberValue;
+    //      end;
+    //    end;
+    //  MainForm.redrawChart;
+    //
+    //  SelectionRange := GetCellRangeString(Selection.Top - 1, Selection.Left - 1, Selection.Bottom - 1, Selection.Right - 1);
+    //  StatusBar.Panels[PANEL_SELECTED_RANGE].Text := SelectionRange;
+    //end;
+  end;
+end;
+
+procedure TMainForm.FileOpenAccept(Sender: TObject);
+var
+  FileOpenAction: TFileOpen;
+begin
+  FileOpenAction:=Sender as TFileOpen;
+  MainForm.FileName:=FileOpenAction.Dialog.FileName;
+  MainForm.WorkbookSource.FileName:=MainForm.FileName;
+end;
+
+procedure TMainForm.ExponentialRadioButtonChange(Sender: TObject);
+begin
+  ExponentialSpan.Enabled := ExponentialRadioButton.Checked;
+end;
+
+procedure TMainForm.ExponentialRadioButtonClick(Sender: TObject);
+begin
+  SmoothingMode := MODE_SIMPLE_EXPONENTIAL;
+  updateSmoothedChart;
+end;
+
+procedure TMainForm.ExponentialSpanChange(Sender: TObject);
+begin
+  updateSmoothedChart;
+end;
+
+procedure TMainForm.GetSelectedDataMenuItemClick(Sender: TObject);
+var
+  Selection: TGridRect;
+  Sheet: TsWorksheet;
+  row, col, SelectedColsAmount, SelectedRowsAmount: Integer;
+  Cell: PCell;
+  SelectionRange: string;
+begin
+  Sheet := WorksheetGrid.WorkbookSource.worksheet;
+  if (not Sheet.IsEmpty) then begin
+    Selection := WorksheetGrid.Selection;
+    SelectedColsAmount := Selection.Right - Selection.Left + 1;
+    SelectedRowsAmount := Selection.Bottom - Selection.Top + 1;
+    if SelectedColsAmount = 2 then
+    begin
+      MainForm.OriginalDataArray := nil;
+      SetLength(MainForm.OriginalDataArray, SelectedRowsAmount, 2);
+      for row := Selection.Top to Selection.Bottom do
+        for col := Selection.Left to Selection.Right do
+        begin
+          Cell := Sheet.FindCell(row - 1 , col - 1);
+          if (Cell <> nil) and (Cell^.ContentType = cctNumber) then
+          begin
+            MainForm.OriginalDataArray[row - Selection.Top, col - Selection.Left] := Cell^.NumberValue;
+          end;
+        end;
+      MainForm.redrawChart;
+
+      SelectionRange := GetCellRangeString(Selection.Top - 1, Selection.Left - 1, Selection.Bottom - 1, Selection.Right - 1);
+      StatusBar.Panels[PANEL_SELECTED_RANGE].Text := SelectionRange;
+    end;
+  end;
+end;
+
+procedure TMainForm.MedianRadioButtonChange(Sender: TObject);
+begin
+  MedianSpan.Enabled := MedianRadioButton.Checked;
+end;
+
+procedure TMainForm.MedianRadioButtonClick(Sender: TObject);
+begin
+  SmoothingMode := MODE_MEDIAN;
+  updateSmoothedChart;
+end;
+
+procedure TMainForm.MedianSpanChange(Sender: TObject);
+begin
+  if not Odd(MedianSpan.Position) then
+  begin
+    if MedianSpan.Position < MedianSpan.Max then
+      MedianSpan.Position := MedianSpan.Position + 1
+    else
+      MedianSpan.Position := MedianSpan.Position - 1;
+  end;
+  updateSmoothedChart;
+end;
+
+procedure TMainForm.MovingAverageRadioButtonChange(Sender: TObject);
+begin
+  MovingAverageSpan.Enabled := MovingAverageRadioButton.Checked;
+end;
+
+procedure TMainForm.MovingAverageRadioButtonClick(Sender: TObject);
+begin
+  SmoothingMode := MODE_MOVING_AVERAGE;
+  updateSmoothedChart;
+end;
+
+procedure TMainForm.MovingAverageSpanChange(Sender: TObject);
+begin
+  if not Odd(MovingAverageSpan.Position) then
+  begin
+    if MovingAverageSpan.Position < MovingAverageSpan.Max then
+      MovingAverageSpan.Position := MovingAverageSpan.Position + 1
+    else
+      MovingAverageSpan.Position := MovingAverageSpan.Position - 1;
+  end;
+  updateSmoothedChart;
+end;
+
+procedure TMainForm.SaveFileExecute(Sender: TObject);
+begin
+  WorkbookSource.SaveToSpreadsheetFile(WorkbookSource.FileName);
+  ShowMessage('File has been saved successfully');
+end;
+
+procedure TMainForm.SelectOutputCellMenuItemClick(Sender: TObject);
+var
+  Sheet: TsWorksheet;
+begin
+  Sheet := WorksheetGrid.WorkbookSource.worksheet;
+  if (not Sheet.IsEmpty) then begin
+    OutputCell := WorksheetGrid.Selection;
+
+    StatusBar.Panels[PANEL_OUTPUT_CELL].Text := GetCellString(OutputCell.Top - 1, OutputCell.Left - 1);
+  end;
+end;
+
+procedure TMainForm.redrawChart();
+var
+  arraySize, i: Integer;
+begin
+  arraySize := length(OriginalDataArray);
+  if arraySize <= 0 then Exit;
+
+  OriginalDataSource.BeginUpdate;
+  OriginalDataSource.Clear;
+  for i := 0 to arraySize - 1 do
+  begin
+    OriginalDataSource.Add(OriginalDataArray[i, 0], OriginalDataArray[i, 1]);
+  end;
+  OriginalDataSource.EndUpdate;
+end;
+
+procedure TMainForm.updateSmoothedChart();
+var
+  maxRow, row, i, arraySize: Integer;
+  X, Y: TDoubleArray;
+begin
+  X := nil;
+  Y := nil;
+  SmoothedY := nil;
+  arraySize := length(OriginalDataArray);
+  if arraySize <= 0 then Exit;
+  SetLength(X, arraySize);
+  SetLength(Y, arraySize);
+
+  for i := 0 to arraySize - 1 do begin
+    X[i] := OriginalDataArray[i, 0];
+    Y[i] := OriginalDataArray[i, 1];
+  end;
+
+  SmoothedDataSource.BeginUpdate;
+  SmoothedDataSource.Clear;
+  case SmoothingMode of
+    MODE_MOVING_AVERAGE: begin
+      MovingAverageMethod(Y, SmoothedY, MovingAverageSpan.Position);
+    end;
+    MODE_SIMPLE_EXPONENTIAL: begin
+      SimpleExponentialMethod(Y, SmoothedY, ExponentialSpan.Position / 10);
+    end;
+    MODE_MEDIAN: begin
+      MedianMethod(Y, SmoothedY, MedianSpan.Position);
+    end;
+  end;
+  if (length(SmoothedY) > 0) then begin
+    for i := 0 to arraySize - 1 do begin
+      SmoothedDataSource.Add(X[i], SmoothedY[i]);
+    end;
+  end;
+  SmoothedDataSource.EndUpdate;
+end;
+
+procedure TMainForm.MovingAverageMethod(const InputData: TDoubleArray; var OutputData: TDoubleArray; const Span: Integer);
+// InputData: входной массив (Y-значения).
+// OutputData: сглаженный массив.
+// Span: ширина окна (должна быть нечетной, например, 3, 5, 7...).
+var
+  i, j, HalfSpan, N, Count: Integer;
+  SumY: Double;
+begin
+  N := Length(InputData);
+  if (N = 0) or (Span < 3) then Exit;
+  // Убедиться, что Span нечетный (если четный, округляем вниз и добавляем 1)
+  HalfSpan := (Span - 1) div 2;
+  SetLength(OutputData, N);
+  // Основной цикл сглаживания
+  for i := 0 to N - 1 do begin
+    SumY := 0.0;
+    Count := 0;
+    // Итерация по окну вокруг текущей точки i (обработка краев)
+    for j := -HalfSpan to HalfSpan do begin
+      if (i + j >= 0) and (i + j < N) then begin
+        SumY := SumY + InputData[i + j];
+        Inc(Count);
+      end;
+    end;
+    // Вычисление среднего
+    if Count > 0 then OutputData[i] := SumY / Count
+    else OutputData[i] := InputData[i];
+    // Оставляем несглаженным, если не удалось найти соседей
+  end;
+end;
+
+procedure TMainForm.SimpleExponentialMethod(const InputData: TDoubleArray;
+  var OutputData: TDoubleArray; const Alpha: Real);
+// InputData: входной массив (Y-значения).
+// OutputData: сглаженный массив.
+// Alpha: коэффициент сглаживания (0.0 < Alpha < 1.0).
+var
+  i, N: Integer;
+begin
+  N := Length(InputData);
+  if (N = 0) or (Alpha <= 0.0) or (Alpha >= 1.0) then Exit;
+  SetLength(OutputData, N);
+  // Инициализация: F_1 = Y_1
+  OutputData[0] := InputData[0];
+  // Итеративная формула: F[t] = Alpha * Y[t-1] + (1 - Alpha) * F[t-1]
+  // Обратите внимание: F[t] в коде - это OutputData[i].
+  for i := 1 to N - 1 do OutputData[i] := Alpha * InputData[i] + (1.0 - Alpha) * OutputData[i - 1];
+end;
+
+procedure TMainForm.MedianMethod(const InputData: TDoubleArray;
+  var OutputData: TDoubleArray; const Span: Integer);
+// InputData: входной массив (Y-значения).
+// OutputData: сглаженный массив.
+// Span: ширина окна (должна быть нечетной, например, 3, 5, 7...).
+var
+  i, j, N, HalfSpan, WindowSize: Integer;
+  Window: TDoubleArray;
+  StartIndex, EndIndex: Integer;
+begin
+  N := Length(InputData);
+  if (N = 0) or (Span < 3) then Exit;
+  HalfSpan := (Span - 1) div 2;
+  SetLength(OutputData, N);
+  for i := 0 to N - 1 do begin
+    // 1. Определение границ скользящего окна
+    StartIndex := i - HalfSpan;
+    if StartIndex < 0 then StartIndex := 0;
+    EndIndex := i + HalfSpan;
+    if EndIndex > N - 1 then EndIndex := N - 1;
+    WindowSize := EndIndex - StartIndex + 1;
+    // 2. Выделение памяти под окно и копирование данных
+    SetLength(Window, WindowSize);
+    for j := 0 to WindowSize - 1 do Window[j] := InputData[StartIndex + j];
+    // 3. Сортировка окна с помощью Generics
+    TArrayHelper<Double>.Sort(Window);
+    // 4. Выбор медианы (центральный элемент)
+    OutputData[i] := Window[WindowSize div 2];
+  end;
+end;
+
+end.
+
